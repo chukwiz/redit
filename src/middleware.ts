07f1e5c09997f63a0cf0121da864cae043a2062c
chukwiz <chukswiz@gmail.com>
2023-07-06 08:22:24 +0100

    optimized  middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,16 @@
-// import { getToken } from "next-auth/jwt";
-// import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function middleware(req: NextRequest) {
-//   const token = await getToken({ req });
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
 
-//   if (!token) {
-//     return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
-//   }
-// }
+  if (!token?.id) {
+    return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
+  }
 
-// export const config = {
-//   matcher: ["/r/:path*/submit", "/r/create"],
-// };
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/r/:path*/submit", "/r/create"],
+};
